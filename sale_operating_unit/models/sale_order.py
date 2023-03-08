e78188ab329bfb73c572a634f66abcb04f9ea72c
Jesús Alan Ramos Rodríguez <alan.ramos@jarsa.com>
2023-03-07 20:04:07 -0600

    [FIX] sale_operating_unit: add store to related field in sale.order.line

--- sale_operating_unit/models/sale_order.py
+++ sale_operating_unit/models/sale_order.py
@@ -89,7 +89,5 @@ class SaleOrderLine(models.Model):
     _inherit = "sale.order.line"
 
     operating_unit_id = fields.Many2one(
-        related="order_id.operating_unit_id",
-        string="Operating Unit",
-        store=True,
+        related="order_id.operating_unit_id", string="Operating Unit", store=True,
     )
