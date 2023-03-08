758682e6af78a780f679347151cb9698efdc8024
Jesús Alan Ramos Rodríguez <alan.ramos@jarsa.com>
2023-03-08 17:32:04 -0600

    [FIX] sale_operating_unit: allow to generate advance invoices with ou

--- sale_operating_unit/wizard/sale_make_invoice_advance.py
+++ sale_operating_unit/wizard/sale_make_invoice_advance.py
@@ -1,4 +1,5 @@
 from odoo import models
+from odoo.exceptions import ValidationError
 
 
 class SaleAdvancePaymentInv(models.TransientModel):
@@ -10,3 +11,26 @@ class SaleAdvancePaymentInv(models.TransientModel):
         )
         invoice.sudo().write({"operating_unit_id": order.operating_unit_id.id})
         return invoice
+
+    def _prepare_invoice_values(self, order, name, amount, so_line):
+        invoice_vals = super(SaleAdvancePaymentInv, self)._prepare_invoice_values(
+            order, name, amount, so_line
+        )
+        journal = self.env["account.journal"].search(
+            [
+                ("operating_unit_id", "=", order.operating_unit_id.id),
+                ("type", "=", "sale"),
+            ],
+            limit=1,
+        )
+        if not journal:
+            raise ValidationError(
+                _("You need to create a sales journal for this operating unit.")
+            )
+        invoice_vals.update(
+            {
+                "operating_unit_id": order.operating_unit_id.id,
+                "journal_id": journal.id,
+            }
+        )
+        return invoice_vals
